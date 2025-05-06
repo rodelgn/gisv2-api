d53ead202eb41920c7576a7c9968332803b7c021
Rodel <rodelg.nercuit@gmail.com>
2025-05-06 16:59:39 +0800

    Created a title table for plotting form

--- db.js
+++ db.js
@@ -28,6 +28,22 @@ const db = {
             email VARCHAR(100) UNIQUE NOT NULL,
             password VARCHAR(100) NOT NULL
         )`);
+
+        await pool.query(`CREATE TABLE IF NOT EXISTS titles (
+            id SERIAL PRIMARY KEY,
+            title_no VARCHAR(100) NOT NULL,
+            title_name VARCHAR(100) NOT NULL,
+            date VARCHAR(100) NOT NULL,
+            survey_no VARCHAR(100) NOT NULL,
+            lot_no VARCHAR(100) NOT NULL,
+            blk_no VARCHAR(100) NOT NULL,
+            area VARCHAR(100) NOT NULL,
+            monument VARCHAR(100) NOT NULL,
+            easting VARCHAR(100) NOT NULL,
+            northing VARCHAR(100) NOT NULL,
+            pluscode VARCHAR(100) NOT NULL
+        )`);
+
     } catch (error) {
         console.error("Error creating table", error);
     }
